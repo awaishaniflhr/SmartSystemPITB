4d572bcb6a165c0e9edbbf774bb16d924e811012
Malik Muhammad Awais <181314@pakaims.edu.pk>
2022-06-23 16:34:47 +0500

    initial commit 2

--- app/src/main/java/com/example/smartsystem_pitb/MainActivity.java
+++ app/src/main/java/com/example/smartsystem_pitb/MainActivity.java
@@ -1,15 +1,65 @@
 package com.example.smartsystem_pitb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Start code for button fragment_home
+        Button btnhome = findViewById(R.id.btnhome);
+        btnhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, HomeFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+            }
+        });
+//        End code for button fragment_home
+
+//        Start code for button fragment_group
+        Button btngroup = findViewById(R.id.btngroup);
+        btngroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, GroupFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+            }
+        });
+
+//        End code for button fragment_group
+
+//        Start code for button fragment_setting
+        Button btnsetting = findViewById(R.id.btnsetting);
+        btnsetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, SettingFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+            }
+        });
+
+//        End code for button fragment_setting
 
     }
 
